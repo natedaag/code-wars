b33d3fbb4871e5ebe41aeee0b2668aebc1b76074
natedaag <natedaage@yahoo.com>
2018-03-29 16:24:52 -0600

    another one done

---
+++ src/com/codewars/XO.java
@@ -0,0 +1,30 @@
+package com.codewars;
+
+public class XO {
+	public static void main(String[] args) {
+		getXO("xxOo");
+	}
+  
+	public static boolean getXO (String str) {
+		str = str.toLowerCase();
+		boolean evenSteven = false;
+		int numX = 0;
+		int numO = 0;
+		for(int i = 0; i < str.length(); i++ ){
+		    //temp = str.charAt(i);
+		    if(str.charAt(i) == 'x') {
+		        numX++;
+		    }
+		    if(str.charAt(i) == 'o') {
+		    	numO++;
+		    }
+		}
+		if(numX == numO) {
+			evenSteven = true;
+		} else {
+			evenSteven = false;
+		}
+		System.out.println(evenSteven);
+		return evenSteven;
+	}
+}
